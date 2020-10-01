b921c3c65d8f3c892b1d1f6fa2829402d191efac
oussema hasnaoui <oussema.hasnaoui@esprit.tn>
2020-10-01 16:39:21 +0100

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,46 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from "./Components/layout/Navbar";
 import Landing from "./Components/layout/Landing";
 import Login from "./Components/auth/Login";
+import Alert from "./Components/layout/Alert"
 import Register from "./Components/auth/Register";
+import Dashboard from "./Components/dashboard/Dashboard";
+import PrivateRoute from "./Components/routing/PrivateRoute";
+//Redux
+import {Provider} from 'react-redux';
+import store from "./store";
 import './App.css';
-const App = () => (
+import setAuthToken from "./utils/setAuthToken";
+import {loadUser} from "./actions/auth";
+
+if (localStorage.token) {
+    setAuthToken(localStorage.token);
+}
+const App = () => {
+    useEffect(() =>{
+        store.dispatch(loadUser());
+
+    }, []);
+    return(
+    <Provider store={store}>
     <Router>
     <Fragment>
         <Navbar></Navbar>
        <Route exact path ='/' component={Landing}/>
        <section className="container">
+           <Alert></Alert>
            <Switch>
                <Route exact path="/Register" component={Register}></Route>
                <Route exact path="/Login" component={Login}></Route>
+               <PrivateRoute exact path="/Dashboard" component={Dashboard} ></PrivateRoute>
 
            </Switch>
        </section>
 
     </Fragment>
     </Router>
-);
+    </Provider>
+)};
 
 export default App;
